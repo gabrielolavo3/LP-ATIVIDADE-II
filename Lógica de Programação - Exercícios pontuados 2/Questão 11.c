806702682fbd9591b9d371d59f0028b4ede98ff2
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-08 13:47:51 -0300

    Update Questão 11.c

--- Lógica de Programação - Exercícios pontuados 2/Questão 11.c
+++ Lógica de Programação - Exercícios pontuados 2/Questão 11.c
@@ -1,3 +1,10 @@
+/* 
+Descrição  : Solicitar ao usuário um código de acesso para entrar em um edifício. 
+O algoritmo deve continuar repetindo até que o usuário digite o código de acesso correto! (função)
+Professor  : Carlos Anderson Santos de Jesus 
+Autor(a)   : Gabriel S. Olavo
+Data atual : 07/11/2023 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,22 +15,26 @@ int main ( ) {
 
 // Declaração de variáveis
 
-    char senhadeAcesso [250] = "Senai522";
+    char senhadeAcesso [250] = "Stone@522";
     char senhaDigitada [250];
 
 // Solicitando dados
 
-    do {
-        printf ("Informe a senha de acesso: ");
-        gets (senhaDigitada);
-
-        if (strcmp (senhadeAcesso, senhaDigitada) != 0) {
-            printf ("\nSenha INVÁLIDA! Insira novamente...\n");  
-        } else {
-            printf ("\nAcesso permitido. BEM-VINDO!\n"); }
-
-    } while (strcmp (senhadeAcesso, senhaDigitada) != 0);
+do {
+    printf ("CENTRO LOGÍSTCO STONE\n\n");
+    printf ("Informe a senha de acesso: ");
+    gets (senhaDigitada);
     
+// Tela de resultado
 
+    if (strcmp (senhadeAcesso, senhaDigitada) != 0) {
+        system ("cls");
+        printf ("Senha INVÁLIDA! Insira novamente...\n\n");  
+    
+    } else {
+        printf ("\nAcesso permitido. BEM-VINDO!\n"); }
+
+} while (strcmp (senhadeAcesso, senhaDigitada) != 0);
+    
     return 0;
 }
